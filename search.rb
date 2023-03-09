767d808dde1e40c0c892f25fdee29d95923b07bd
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-03-09 18:04:27 +0300

    Build the graph search

---
+++ search.rb
@@ -0,0 +1,93 @@
+require 'json'
+require 'set'
+
+require_relative './lib/friends_graph'
+
+test_data_file = File.read('./data.json')
+data = JSON.parse(test_data_file)
+
+module FriendsType
+  DIRECT = 'direct'.freeze
+end
+
+class Search
+  ZERO = 0
+
+  attr_accessor :graph
+
+  def initialize(graph)
+    @graph = graph
+    @nodes_collection = []
+    @current_depth_level = ZERO
+    @list = []
+  end
+
+  def call(params)
+    breadth_first(ZERO, params)
+    depth_first(graph, params)
+
+    puts nodes_collection.map(&:value).inspect
+  end
+
+  def friends_distribution_by_country(
+    opts = { friend: nil, friends_type: FriendsType::DIRECT }
+  )
+    call(opts)
+    results =
+      nodes_collection
+        .to_a
+        .group_by { |i| i.country }
+        .transform_values { |values| values.count}
+    puts results
+  end
+
+  private
+
+  attr_accessor :nodes_collection,
+                :current_depth_level,
+                :list
+
+  def breadth_first(
+    index = ZERO,
+    filters = { friend: nil, friends_type: 'DIRECT' }
+  )
+    nodes_collection << graph if index == ZERO
+    root = nodes_collection[index] rescue nil
+    return if root.nil?
+    root_nodes = root.nodes
+
+    track_depth_level(index, root_nodes) do
+      root_nodes.each do |node|
+        next if nodes_collection.include? node
+        nodes_collection << node
+      end
+
+      breadth_first((index+1), filters)
+    end
+  end
+
+  def depth_first(node = graph, filters = {})
+    return if nodes_collection.include? node
+    nodes_collection << node
+    root_nodes = node.nodes
+    root_nodes.each do |node|
+      depth_first(node, filters)
+    end
+  end
+
+  def track_depth_level(index, root_nodes)
+    yield
+  end
+
+end
+
+# Search for Jamie ́s friends distribution by Country ex: US: 2, ES:3
+
+graph = FriendsGraph.new({root_node: 'tom UK'}).build(data)
+search = Search.new(graph)
+
+search.call({})
+
+# search.friends_distribution_by_country(
+#   { friend: 'jamie' }.merge({ friends_type: FriendsType::DIRECT })
+# )
