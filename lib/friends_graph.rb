4011b8545f74b609e2d3e62e3461fadf73a714ba
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-03-09 13:48:42 +0300

    Build friends graph with root node set

---
+++ lib/friends_graph.rb
@@ -0,0 +1,19 @@
+require 'json'
+require_relative './builders/graph_builder'
+
+class FriendsGraph
+
+  def initialize(opts = { root_node: ''})
+    @root_node = opts[:root_node]
+  end
+
+  def build(data = {})
+    GraphBuilder.build(data) do |builder|
+      builder.set_root_node(root_node)
+    end
+  end
+
+  private
+
+  attr_accessor :root_node
+end
